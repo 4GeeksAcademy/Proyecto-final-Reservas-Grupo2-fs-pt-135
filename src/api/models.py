cb113803a20773742e7722058057e0fa9a31aa0f
wladimiredgar <wladimiredgar@gmail.com>
2026-06-11 20:10:30 +0000

    feat(auth): add register and login endpoints

--- src/api/models.py
+++ src/api/models.py
@@ -7,7 +7,10 @@ db = SQLAlchemy()
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    phone: Mapped[str] = mapped_column(String(20), nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
+
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
@@ -15,5 +18,8 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "phone": self.phone,
+            "is_active": self.is_active,
             # do not serialize the password, its a security breach
         }
