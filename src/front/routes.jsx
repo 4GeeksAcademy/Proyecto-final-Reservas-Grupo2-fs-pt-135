cb00a48aa895c75829b45d5e8742f4a1af68cc8a
fran81caco <cacodreamer81@gmail.com>
2026-06-22 16:12:10 +0000

    Arreglo de rutas y carga final del Landing Page

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -7,7 +7,6 @@ import {
 } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import { Login } from "./pages/Login";
 import { RegisterClient } from "./pages/RegisterClient";
@@ -19,7 +18,6 @@ export const router = createBrowserRouter(
     <>
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>}>
         <Route path="/" element={<Home />} />
-        <Route path="/single/:theId" element={<Single />} />
         <Route path="/demo" element={<Demo />} />
       </Route>
 
